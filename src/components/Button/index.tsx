47815df5fe57b69893d99d8d568d7c06c1a65436
ttnguyen115 <thanhtrung1012000@gmail.com>
2023-12-16 20:57:34 +0700

    add storybook and create button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,9 @@
-const Button = () => {
-  return <button className="text-blue-500">Button</button>;
+import { ComponentProps } from "react";
+
+type ButtonPropps = ComponentProps<"button">;
+
+const Button = ({ ...props }: ButtonPropps) => {
+  return <button className="text-blue-500" {...props} />;
 };
 
 export default Button;
