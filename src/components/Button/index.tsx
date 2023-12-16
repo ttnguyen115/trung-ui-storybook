a3b21d01069455f13ad1d905aa55b64e262c1a41
ttnguyen115 <thanhtrung1012000@gmail.com>
2023-12-16 21:26:17 +0700

    done button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,70 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/utils";
 
-type ButtonPropps = ComponentProps<"button">;
+const buttonStyles = cva(
+  [
+    "w-full",
+    "rounded-md",
+    "font-semibold",
+    "focus:outline-none",
+    "disabled:cursor-not-allowed",
+  ],
+  {
+    variants: {
+      variant: {
+        solid: "",
+        outline: "border-2",
+        ghost: "transition-colors duration-300",
+      },
+      size: {
+        sm: "px-4 py-2 text-sm",
+        md: "px-4 py-2 text-base",
+        lg: "px-6 py-3 text-lg",
+      },
+      colorScheme: {
+        primary: "text-white",
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "solid",
+        colorScheme: "primary",
+        className: "bg-primary-500 hover:bg-primary-600",
+      },
+      {
+        variant: "outline",
+        colorScheme: "primary",
+        className:
+          "text-primary-600 border-primary-500 bg-transparent" +
+          " hover:bg-primary-100",
+      },
+      {
+        variant: "ghost",
+        colorScheme: "primary",
+        className: "text-primary-600 bg-transparent hover:bg-primary-100",
+      },
+    ],
+    defaultVariants: {
+      variant: "solid",
+      size: "md",
+      colorScheme: "primary",
+    },
+  }
+);
 
-const Button = ({ ...props }: ButtonPropps) => {
-  return <button className="text-blue-500" {...props} />;
-};
+type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant, size, colorScheme, className, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(buttonStyles({ variant, size, colorScheme, className }))}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Button;
