c42f69b86192ca5d911cd78d6ea585cb2a540379
ttnguyen115 <thanhtrung1012000@gmail.com>
2023-12-16 22:03:25 +0700

    add input field component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,36 @@
+import { ComponentProps, forwardRef } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/utils";
+
+const inputStyles = cva([
+  "w-full",
+  "border",
+  "border-gray-200",
+  "p-2",
+  "rounded-lg",
+  "transition-all",
+  "duration-100",
+  "outline-none",
+  "focus:outline-primary-500 ",
+  "focus:border-transparent",
+  "placeholder:text-gray-400",
+  "placeholder:text-sm",
+]);
+
+type InputProps = ComponentProps<"input"> & VariantProps<typeof inputStyles>;
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type="text"
+        autoComplete="off"
+        className={cn(inputStyles(), className)}
+        {...props}
+      />
+    );
+  }
+);
+
+export default Input;
